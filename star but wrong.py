0c68d82bd2699046d5b6345e6a12389ca3eadbea
swimdoraemon <indoaradhya@gmail.com>
2024-09-18 20:39:59 +0900

    star

---
+++ star but wrong.py
@@ -0,0 +1,35 @@
+import turtle
+
+
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Screen()
+sc.setup(400,300)
+
+turtle.title("Welcome to turtule window")
+
+
+t = turtle.Turtle()
+
+t.fd(50)
+t.rt(120)
+t.fd(50)
+t.rt(120)
+t.fd(50)
+t.pu()
+
+t.pd()
+
+t.fd(50)
+t.rt(120)
+t.fd(50)
+t.rt(120)
+t.fd(50)
+t.pu()
+
+
+
+
+
+
+turtle.done()
---
+++ star.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+turtle.Screen().bgcolor("orange")
+
+sc = turtle.Screen()
+sc.setup(400,300)
+
+turtle.title("Welcome to turtule window")
+
+
+t = turtle.Turtle()
+
+
+for i in range(5):
+    t.fd(150)
+    t.left(146)
+    i =i+1
